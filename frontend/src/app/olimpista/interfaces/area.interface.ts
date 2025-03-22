79d036cce65b8c6550e281e552b35d3d2778fe08
Anahi44 <gamboaanahi46@gmail.com>
2025-03-22 14:34:03 -0400

    Creacion del boton de añadir categoria1

--- frontend/src/app/olimpista/interfaces/area.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Area {
-  id: number;
-  nombre_area: string;
-  costo: number;
-  createdAt: Date;
-  updatedAt: Date;
-}
--- frontend/src/app/olimpista/interfaces/categoria.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface NivelesCategoria {
-  id:          number;
-  nombre:      string;
-  descripcion: string;
-  createdAt:   Date;
-  updatedAt:   Date;
-}
