cb59eadb1a7e2c02d1c6c23d9c8097a9447761a3
Andy31bm <andrea10borda@gmail.com>
2025-03-23 19:40:53 -0300

    Card para mostrar categorias

--- TIS_FRONTEND/src/app/olimpista/components/areas-card/areas-card.component.ts
+++ TIS_FRONTEND/src/app/olimpista/components/areas-card/areas-card.component.ts
@@ -4,26 +4,54 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-areas-card',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './areas-card.component.html',
 })
 export class AreasCardComponent {
-      @Input({required:true})
-      Area!:Area;
+  @Input({ required: true }) Area!: Area;
 
-      isModalOpen = false;
+  isModalOpen = false;
+  isConfirmModalOpen = false;
+  cards: { nombre_area: string; descripcion: string }[] = [];
+  cardIndexToDelete: number | null = null; // Nueva propiedad para almacenar el índice de la tarjeta
 
-      openModal() {
-        this.isModalOpen = true; 
-      }
-    
-      closeModal() {
-        this.isModalOpen = false;
-      }
-    
-      saveCategory() {
-        console.log('Categoría guardada');
-        this.closeModal(); 
-      }
+  openModal() {
+    this.isModalOpen = true;
+  }
 
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  saveCategory() {
+    console.log('Categoría guardada');
+
+    // Agregar una nueva tarjeta al array
+    this.cards.push({
+      nombre_area: this.Area.nombre_area,
+      descripcion: this.Area.descripcion || 'No disponible',
+    });
+
+    this.closeModal();
+  }
+   openConfirmModal(index: number) {
+    this.cardIndexToDelete = index; // Almacenar el índice de la tarjeta
+    this.isConfirmModalOpen = true;
+  }
+
+  closeConfirmModal() {
+    this.isConfirmModalOpen = false;
+    this.cardIndexToDelete = null; // Limpiar el índice almacenado
+  }
+  disableArea() {
+    if (this.cardIndexToDelete !== null) {
+      console.log(`Tarjeta en índice ${this.cardIndexToDelete} deshabilitada`);
+
+      // Eliminar la tarjeta usando su índice
+      this.cards.splice(this.cardIndexToDelete, 1);
+
+      this.closeConfirmModal();
+    }
+  }
 }
