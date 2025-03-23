5fe548eb7ca04fbc76d207e55e4095e3397a2639
Anahi44 <gamboaanahi46@gmail.com>
2025-03-23 10:27:16 -0400

    Union de modal y JOSUE_METAL

--- TIS_FRONTEND/src/app/olimpista/components/areas-card/areas-card.component.ts
+++ TIS_FRONTEND/src/app/olimpista/components/areas-card/areas-card.component.ts
@@ -4,16 +4,27 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-areas-card',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './areas-card.component.html',
 })
 export class AreasCardComponent {
-
-
-
       @Input({required:true})
       Area!:Area;
 
+      isModalOpen = false;
 
+      openModal() {
+        this.isModalOpen = true; 
+      }
+    
+      closeModal() {
+        this.isModalOpen = false;
+      }
+    
+      saveCategory() {
+        console.log('Categoría guardada');
+        this.closeModal(); 
+      }
 
 }
