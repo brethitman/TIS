b003e59de8d10583f489a28ed554b5b582083606
WooGruvia <vegef40@gmail.com>
2025-04-13 16:26:20 -0400

    Union con edit

--- TIS_FRONTEND/src/app/olimpista/components/olimpiada-card/olimpiada-card.component.ts
+++ TIS_FRONTEND/src/app/olimpista/components/olimpiada-card/olimpiada-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Olimpiada } from '../../interfaces/olimpiada.interfacel';
 import { Router } from '@angular/router';
 import { OlimpiadaService } from '../../service/olimpiada.service';
@@ -7,7 +8,7 @@ import { OlimpiadaService } from '../../service/olimpiada.service';
 @Component({
   selector: 'app-olimpiada-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './olimpiada-card.component.html',
 })
 export class OlimpiadaCardComponent implements OnInit {
@@ -18,6 +19,20 @@ export class OlimpiadaCardComponent implements OnInit {
   isModalOpen = false;
   isDeleteModalOpen = false;
   isEditModalOpen = false;
+  errorMessage: string = '';
+
+  // Editable: versión del objeto con fechas como string
+  editableOlimpiada: {
+    nombre_olimpiada: string;
+    descripcion_olimpiada: string;
+    fecha_inicio: string;
+    fecha_final: string;
+  } = {
+    nombre_olimpiada: '',
+    descripcion_olimpiada: '',
+    fecha_inicio: '',
+    fecha_final: ''
+  };
 
    // Estados para modales
    mostrarConfirmacion = false;
@@ -87,7 +102,38 @@ export class OlimpiadaCardComponent implements OnInit {
   }
 
   onButton2Click(): void {
-    console.log('Botón 2 clickeado');
+    this.editableOlimpiada = {
+      nombre_olimpiada: this.olimpiada.nombre_olimpiada,
+      descripcion_olimpiada: this.olimpiada.descripcion_olimpiada,
+      fecha_inicio: this.formatDateToInput(this.olimpiada.fecha_inicio),
+      fecha_final: this.formatDateToInput(this.olimpiada.fecha_final),
+    };
+    this.errorMessage = '';
+    this.isEditModalOpen = true;
+  }
+
+  // Guardar cambios y validar
+  saveEdit(): void {
+    const inicio = new Date(this.editableOlimpiada.fecha_inicio);
+    const fin = new Date(this.editableOlimpiada.fecha_final);
+
+    if (fin < inicio) {
+      this.errorMessage = 'La fecha final debe ser posterior o igual a la fecha de inicio.';
+      return;
+    }
+
+    // Actualizar objeto original
+    this.olimpiada.nombre_olimpiada = this.editableOlimpiada.nombre_olimpiada;
+    this.olimpiada.descripcion_olimpiada = this.editableOlimpiada.descripcion_olimpiada;
+    this.olimpiada.fecha_inicio = inicio;
+    this.olimpiada.fecha_final = fin;
+
+    this.isEditModalOpen = false;
+  }
+  
+  formatDateToInput(date: Date): string {
+    const d = new Date(date);
+    return d.toISOString().split('T')[0]; // YYYY-MM-DD
   }
 
   onButton3Click(): void {
