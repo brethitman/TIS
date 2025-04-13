82a3ff457e1f5a577234df7fd4c7e33a6e94503a
elena <bernabemary0101@gmail.com>
2025-04-13 09:48:33 -0400

    actualizar

--- TIS_FRONTEND/src/app/olimpista/components/olimpiada-list/olimpiada-list.component.ts
+++ TIS_FRONTEND/src/app/olimpista/components/olimpiada-list/olimpiada-list.component.ts
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
-// olimpiada-list.component.ts
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { OlimpiadaService } from '../../service/olimpiada.service';
-=======
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OlimpiadaCardComponent } from '../olimpiada-card/olimpiada-card.component';
->>>>>>> 05eb073ebb696730264662f01b4a778f54adf17f
 import { Olimpiada } from '../../interfaces/olimpiada.interfacel';
-import { OlimpiadaCardComponent } from "../olimpiada-card/olimpiada-card.component";
+
 
 @Component({
   standalone: true,
   selector: 'app-olimpiada-list',
-<<<<<<< HEAD
-  templateUrl: './olimpiada-list.component.html',
-  imports: [CommonModule, OlimpiadaCardComponent]
-=======
-  standalone: true,
   imports: [CommonModule, OlimpiadaCardComponent],
   templateUrl: './olimpiada-list.component.html',
->>>>>>> 05eb073ebb696730264662f01b4a778f54adf17f
 })
 export class OlimpiadaListComponent {
   @Input({ required: true }) olimpiadas!: Olimpiada[];
