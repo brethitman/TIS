63125e629bf518a2b6854686807b2c8d78ef4c0f
Anahi44 <gamboaanahi46@gmail.com>
2025-04-28 09:59:19 -0400

    Guardando todo antes de instalar xls

---
+++ TIS_FRONTEND/src/app/olimpista/components/boton-exel/boton-exel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-exel',
+  imports: [],
+  templateUrl: './boton-exel.component.html',
+  styleUrl: './boton-exel.component.css'
+})
+export class BotonExelComponent {
+
+}
