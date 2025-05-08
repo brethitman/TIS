d309b1c2441edfb33ab1d4e33c6c53b952d939bb
Anahi44 <gamboaanahi46@gmail.com>
2025-05-07 20:32:56 -0400

    Commit para la union de lenny con el mio

--- TIS_FRONTEND/src/app/olimpista/pages/ventana-informacion-olimpiada/ventana-informacion-olimpiada.component.ts
+++ TIS_FRONTEND/src/app/olimpista/pages/ventana-informacion-olimpiada/ventana-informacion-olimpiada.component.ts
@@ -1,31 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { VisualizacionPageResponse } from '../../interfaces/olimpiadaVisualizacion.interface';
+import { VisualizacionPageResponse, Area } from '../../interfaces/olimpiadaVisualizacion.interface';
 import { VisualizacionService } from '../../service/Visualizacion.service';
-import { Olimpiada } from '../../interfaces/olimpiada-interfase';
-import { IDOlimpiadabyArea, NivelCategoria } from '../../interfaces/olimpiadaAreaCategoria.interface';
-import { Subject, takeUntil } from 'rxjs';
-import { OlimpiadaByAreaService } from '../../service/OlimpiadaByArea.service';
+import { AreasCarruselComponent } from '../../components/areas-carrusel/areas-carrusel.component';
 
 @Component({
   selector: 'app-ventana-informacion-olimpiada',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AreasCarruselComponent],
   templateUrl: './ventana-informacion-olimpiada.component.html',
 })
-export class VentanaInformacionOlimpiadaComponent implements OnInit {
+export class VentanaInformacionOlimpiadaComponent  {
 
   olimpiada: any;
   olimpiadaId: number;
-  areas: any[] = [];
+  public Area = signal<Area[]>([]);
   confirmacion: boolean = false;
-  private destroy$ = new Subject<void>();
-  areasDisponibles: IDOlimpiadabyArea[] = [];
-  errorMessage: string | null = null;
+  areasDisponibles: any;
 
   constructor(private route: ActivatedRoute, private router: Router,
-     private servicio: VisualizacionService,private olimpiadaByAreaService: OlimpiadaByAreaService,) {
+     private servicio: VisualizacionService) {
     const navigation = this.router.getCurrentNavigation();
     const stateData = navigation?.extras.state as { [key: string]: any };
 
@@ -35,28 +30,15 @@ export class VentanaInformacionOlimpiadaComponent implements OnInit {
       console.error('No se recibió información de la olimpiada');
     }
   }
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-        const olimpiadaId = params['id'];
-        if (olimpiadaId) {
-          this.cargarAreas(olimpiadaId);
-        } else {
-          console.error('No se encontró ID de olimpiada en la URL');
-          // Puedes redirigir a una página de error
-        }
-      });
+
+  cargarAreas(olimpiadaId: number): void {
+    this.servicio.getAreasXOlimpiada(olimpiadaId).subscribe({
+      next: (areas) => this.Area.set(areas),
+      error: (error) => console.error('Error al obtener áreas:', error)
+    });
+    console.log('envio de areas:', this.areasDisponibles,'areas: ', this.Area)
   }
-  private cargarAreas(olimpiadaId: string): void {
-    this.olimpiadaByAreaService.getAreasByOlimpiadaId(Number(olimpiadaId))
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (areas) => this.areasDisponibles = areas,
-        error: (error) => {
-          console.error('Error cargando áreas:', error);
-          this.errorMessage = 'Error al cargar las áreas disponibles';
-        }
-      });
-  }
+  
   entrar(): void {
     if (!this.olimpiada?.id) {
       console.error('Error: No se puede navegar - Olimpiada sin ID');
