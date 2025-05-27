5dd020d192fdf2ca1a087b111c5617bf69335cc9
Andy31bm <andrea10borda@gmail.com>
2025-05-27 16:54:20 -0300

    Lista de estudiantes inscritos

--- TIS_FRONTEND/src/app/olimpista/pages/boleta-lista/boleta-lista.component.ts
+++ TIS_FRONTEND/src/app/olimpista/pages/boleta-lista/boleta-lista.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-boleta-lista',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './boleta-lista.component.html'
 })
 export class BoletaListaComponent {
+  // Datos de ejemplo
+  students = [
+    {
+      id: 1,
+      name: 'Colacanuto Cornamenta',
+      course: 'Matemáticas Avanzadas',
+      area1: 'Ciencias',
+      category1: 'Sub-17',
+      area2: 'Robótica',
+      category2: 'Principiante'
+    },
+    {
+      id: 2,
+      name: 'Barbara Sprouse',
+      course: 'Literatura Moderna',
+      area1: 'Humanidades',
+      category1: 'Avanzado',
+      area2: 'Teatro',
+      category2: 'Intermedio'
+    }
+  ];
 
 }
