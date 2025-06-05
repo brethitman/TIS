6ea539a8ec3625c69ce1dbd0ed157e9d90eec91c
mlfc0106 <fernandez.campos.maria.laura@gmail.com>
2025-06-05 09:56:44 -0400

    Arreglo de reportes

--- TIS_BACKEND/app/Http/Controllers/Inscripcion/InscripcionController.php
+++ TIS_BACKEND/app/Http/Controllers/Inscripcion/InscripcionController.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
-use App\Models\Curso;
+
 
 class InscripcionController extends Controller
 {
